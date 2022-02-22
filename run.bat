dd41a8cbfcc0cc92f41072831f686626f8e65d9a
pagepan <pagepan@qq.com>
2022-02-22 11:21:57 +0800

    增加中文字体和修复批处理生成css文件utf8编码

--- run.bat
+++ run.bat
@@ -36,50 +36,58 @@ for /d %%d in (*) do (
     for %%i in (a b c d e f g h i j k l m n o p q r s t u v w x y z) do call set lower_dir=%%lower_dir:%%i=%%i%%
     
     set fontmin_dir="%root_dir%font-min\!lower_dir!"
-	if exist "!fontmin_dir!" rd /s /q "!fontmin_dir!"
-    md "!fontmin_dir!"
+	if not exist "!fontmin_dir!" (
     
-    set web_dir="%root_dir%web\!lower_dir!"
-	if exist "!web_dir!" rd /s /q "!web_dir!"
-    md "!web_dir!"
-    
-    cd %root_dir%font-src\%%d
-    
-    for /f "delims=" %%f in ('dir /b /a-d "*.ttf"') do (
+        md "!fontmin_dir!"
         
-        set filename=%%~nf
+        set web_dir="%root_dir%web\!lower_dir!"
         
-        if not "!filename:-Heavy=!"=="!filename!" (
-            set font_weight=900
-        ) else if not "!filename:-Black=!"=="!filename!" (
-            set font_weight=900
-        ) else if not "!filename:-ExtraBold=!"=="!filename!" (
-            set font_weight=800
-        ) else if not "!filename:-Bold=!"=="!filename!" (
-            set font_weight=700
-        ) else if not "!filename:-Semibold=!"=="!filename!" (
-            set font_weight=600
-        ) else if not "!filename:-Demibold=!"=="!filename!" (
-            set font_weight=600
-        ) else if not "!filename:-Medium=!"=="!filename!" (
-            set font_weight=500
-        ) else if not "!filename:-Normal=!"=="!filename!" (
-            set font_weight=400
-        ) else if not "!filename:-Regular=!"=="!filename!" (
-            set font_weight=400
-        ) else if not "!filename:-Light=!"=="!filename!" (
-            set font_weight=300
-        ) else if not "!filename:-ExtraLight=!"=="!filename!" (
-            set font_weight=200
-        ) else if not "!filename:-Thin=!"=="!filename!" (
-            set font_weight=100
+        if exist "!web_dir!" rd /s /q "!web_dir!"
+        
+        md "!web_dir!"
+        
+        cd %root_dir%font-src\%%d
+        
+        for /f "delims=" %%f in ('dir /b /a-d "*.ttf"') do (
+            
+            set filename=%%~nf
+            
+            if not "!filename:-Heavy=!"=="!filename!" (
+                set font_weight=900
+            ) else if not "!filename:-Black=!"=="!filename!" (
+                set font_weight=900
+            ) else if not "!filename:-ExtraBold=!"=="!filename!" (
+                set font_weight=800
+            ) else if not "!filename:-Bold=!"=="!filename!" (
+                set font_weight=700
+            ) else if not "!filename:-Semibold=!"=="!filename!" (
+                set font_weight=600
+            ) else if not "!filename:-Demibold=!"=="!filename!" (
+                set font_weight=600
+            ) else if not "!filename:-Medium=!"=="!filename!" (
+                set font_weight=500
+            ) else if not "!filename:-Normal=!"=="!filename!" (
+                set font_weight=400
+            ) else if not "!filename:-Regular=!"=="!filename!" (
+                set font_weight=400
+            ) else if not "!filename:-Light=!"=="!filename!" (
+                set font_weight=300
+            ) else if not "!filename:-ExtraLight=!"=="!filename!" (
+                set font_weight=200
+            ) else if not "!filename:-Thin=!"=="!filename!" (
+                set font_weight=100
+            )
+            
+            pyftsubset %root_dir%font-src\%%d\%%~nf.ttf --output-file=%root_dir%font-min\%%d\%%~nf.ttf --text-file=%root_dir%!text_file!.txt --no-hinting
+            
+            fonttools ttLib.woff2 compress -o "%root_dir%web\!lower_dir!\%%~nf.woff2" "%root_dir%font-min\!lower_dir!\%%~nf.ttf"
+            
+            set cssfile=%root_dir%web\!lower_dir!\!lower_dir!.css
+            
+            set csscontent=@font-face{font-family:''%%d'';src:url^(''!filename!.woff2''^) format^(''woff2''^);font-weight:!font_weight!;font-style:normal;font-display:swap;}
+            
+            pwsh -Command "'!csscontent!' | Out-File ('!cssfile!') -Encoding utf8NoBOM"
         )
-        
-        echo ^@font-face{font-family:'%%d';src:url^('!filename!.woff2'^) format^('woff2'^);font-weight:!font_weight!;font-style:normal;font-display:swap;} >> %root_dir%web\!lower_dir!\!lower_dir!.css
-    
-        pyftsubset %root_dir%font-src\%%d\%%~nf.ttf --output-file=%root_dir%font-min\%%d\%%~nf.ttf --text-file=%root_dir%!text_file!.txt --no-hinting
-        
-        fonttools ttLib.woff2 compress -o "%root_dir%web\!lower_dir!\%%~nf.woff2" "%root_dir%font-min\!lower_dir!\%%~nf.ttf"
     )
 )
 
