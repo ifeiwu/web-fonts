26092c554737e231c40c03417615aad770745465
pagepan <pagepan@qq.com>
2022-03-06 18:34:52 +0800

    增加多种字数

--- run.bat
+++ run.bat
@@ -16,25 +16,30 @@ set /p num=请输入编号：
 
 set end_msg=网页字体转换完成，请在 web 目录下查看。
 
-set text_file=zh-cn-3700
+rem set text_file=zh-cn-3700
+set text_lang=zh-cn
+set text_word=3700
+
 
 set font_src_lang=zh
-set font_min_lang=zh-cn
 
 if /i "!num!"=="2" (
-	set text_file=zh-cn-3500
+    set text_lang=zh-cn
+    set text_word=3500
 ) else if /i "!num!"=="3" (
-	set text_file=zh-cn-2000
+    set text_lang=zh-cn
+    set text_word=2000
 ) else if /i "!num!"=="4" (
-	set text_file=zh-cn-7000
+    set text_lang=zh-cn
+    set text_word=7000
 ) else if /i "!num!"=="5" (
-	set text_file=zh-cn-tw-5000
-    set font_src_lang=zh
-    set font_min_lang=zh-cn-tw
+    set text_lang=zh-cn-tw
+    set text_word=5000
 ) else if /i "!num!"=="6" (
 	set font_src_lang=en
-	set font_min_lang=en
 )
+
+set text_file=!text_lang!-!text_word!
 
 if not "!font_src_lang!"=="en" (
     echo.
@@ -43,29 +48,28 @@ if not "!font_src_lang!"=="en" (
 )
 
 set root_dir=%~dp0
+set fontsrc_dir=!root_dir!font-src\!font_src_lang!
 
-cd font-src\%font_src_lang%
+cd !fontsrc_dir!
 
 for /d %%d in (*) do (
 
     set lower_dir=%%d
 
     for %%i in (a b c d e f g h i j k l m n o p q r s t u v w x y z) do call set lower_dir=%%lower_dir:%%i=%%i%%
+
+    set css_dir="!root_dir!css\!text_word!\!text_lang!\!lower_dir!"
     
-    set fontmin_dir="%root_dir%font-min\%font_min_lang%\!lower_dir!"
+	if not exist "!css_dir!" (
     
-	if not exist "!fontmin_dir!" (
-    
-        md "!fontmin_dir!"
+        md "!css_dir!"
         
-        set web_dir="%root_dir%css\!lower_dir!"
+        set fontmin_dir=!root_dir!font-min\%%d
         
-        if exist "!web_dir!" rd /s /q "!web_dir!"
+        if not exist "!fontmin_dir!" md "!fontmin_dir!"
         
-        md "!web_dir!"
-        
-        cd %root_dir%font-src\%font_src_lang%\%%d
-        
+        cd !fontsrc_dir!\%%d
+
         for /f "delims=" %%f in ('dir /b /a-d "*.ttf"') do (
             
             set filename=%%~nf
@@ -98,11 +102,11 @@ for /d %%d in (*) do (
                 set font_weight=100
             )
             
-            pyftsubset %root_dir%font-src\%font_src_lang%\%%d\%%~nf.ttf --output-file=%root_dir%font-min\%font_min_lang%\%%d\%%~nf.ttf --text-file=%root_dir%!text_file!.txt --no-hinting
+            pyftsubset !fontsrc_dir!\%%d\%%~nf.ttf --output-file=!fontmin_dir!\%%~nf.ttf --text-file=!root_dir!!text_file!.txt --no-hinting
             
-            fonttools ttLib.woff2 compress -o "%root_dir%css\!lower_dir!\%%~nf.woff2" "%root_dir%font-min\%font_min_lang%\!lower_dir!\%%~nf.ttf"
-            
-            set cssfile=%root_dir%css\!lower_dir!\!lower_dir!.css
+            fonttools ttLib.woff2 compress -o "!css_dir!\%%~nf.woff2" "!fontmin_dir!\%%~nf.ttf"
+
+            set cssfile=!css_dir!\!lower_dir!.css
             
             set csscontent=@font-face{font-family:''%%d'';src:url^(''!filename!.woff2''^) format^(''woff2''^);font-weight:!font_weight!;font-style:normal;font-display:swap;}
             
@@ -110,6 +114,8 @@ for /d %%d in (*) do (
         )
     )
 )
+
+if exist "!root_dir!font-min" rd /s /q "!root_dir!font-min"
 
 echo.
 echo !end_msg!
